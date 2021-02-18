535d2cfb01320f33e588f6c0a551b269a413aa10
SDoehren <samdoehren@gmail.com>
2021-02-18 13:48:06 +0000

    v0.1.0

---
+++ README.md
@@ -0,0 +1,37 @@
+# Beast Arena
+
+[![GitHub release](https://img.shields.io/github/release/sdoehren/beast-arena.svg)](https://GitHub.com/sdoehren/always-centred/releases/)
+
+[![Forge Install %](https://img.shields.io/badge/dynamic/json?label=Forge%20Installs&query=package.installs&suffix=%25&url=https%3A%2F%2Fforge-vtt.com%2Fapi%2Fbazaar%2Fpackage%2Fbeast-arena
+)](https://GitHub.com/sdoehren/beast-arena/releases/)
+
+
+[![Github all releases](https://img.shields.io/github/downloads/sdoehren/beast-arena/total.svg)](https://GitHub.com/sdoehren/beast-arena/releases/)
+[![Github Releases (by Release)](https://img.shields.io/github/downloads/sdoehren/beast-arena/v0.1.0/total.svg)](https://GitHub.com/sdoehren/beast-arena/releases/)
+
+
+
+[![GitHub issues](https://img.shields.io/github/issues/sdoehren/beast-arena.svg)](https://GitHub.com/sdoehren/beast-arena/issues/)
+ 
+
+[![ko-fi](https://www.ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/sdoehren)
+
+
+## Instructions
+
+
+## Settings
+
+
+## Known issues
+
+
+
+## Changes under consideration
+
+
+
+## Change log
+
+#### 0.1.0 - First Beta Release  
+This was the first public beta.
---
+++ src/settings.js
@@ -0,0 +1,146 @@
+
+
+export const registerSettings = function () {
+
+
+    game.settings.register("always-centred", "mode", {
+        name: "Mode:",
+        hint:"Disabled: Module off (default) | Party View: all player owned tokens | Selected Token: only the selected token.",
+        scope: "client",
+        config: true,
+        default: "disabled",
+        type: String,
+        choices: {disabled: "Disabled", pcs: "Party View", selectedtoken: "Selected Token"},
+    });
+
+    game.settings.register("always-centred", "autozoom", {
+        name: "Auto Zoom",
+        hint:"Adjust zoom as well as position. Highly Recommended.",
+        scope: "client",
+        config: true,
+        default: true,
+        type: Boolean
+    });
+
+    game.settings.register("always-centred", "mitigatebounce", {
+        name: "Mitigate Bouncing Issue",
+        hint:"Only relevant when Auto Zoom is set to off and Mode is Party View. Forces Auto Zoom when required.",
+        scope: "world",
+        config: true,
+        default: true,
+        type: Boolean
+    });
+
+    game.settings.register("always-centred", "paddingsq", {
+        name: "Padding (squares)",
+        hint:"Padding added to view area in all directions. Highly Recommended to be set to >2 and will have no effect <1.",
+        scope: "client",
+        config: true,
+        default: 12,
+        type: Number
+    });
+
+    game.settings.register("always-centred", "paddingper", {
+        name: "Padding (%)",
+        hint:"Percentage increase of view area",
+        scope: "client",
+        config: true,
+        default: 33,
+        type: Number
+    });
+
+    game.settings.register("always-centred", "maxzoom", {
+        name: "Max zoom level",
+        hint:"",
+        scope: "client",
+        config: true,
+        default: 1,
+        type: Number
+    });
+
+    game.settings.register("always-centred", "updatespeed", {
+        name: "Camera animation speed",
+        hint:"",
+        scope: "client",
+        config: true,
+        default: 500,
+        type: Number
+    });
+
+    game.settings.register("always-centred", "includeinvisible", {
+        name: "Include Invisible",
+        hint: "Whether to account for tokens that can't be seen by the user in the party view calculations.",
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean
+    });
+
+    game.settings.register("always-centred", "DMControl", {
+        name: "GM Control all users pan and zoom?",
+        hint:"Allows the GM to control player centring (this mode assumes all players and GM are viewing the same scene)",
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean
+    });
+
+    game.settings.register("always-centred", "Button-GMControl", {
+        name: "Button - GMControl",
+        hint: "Toggles button visiblity",
+        scope: "client",
+        config: true,
+        default: false,
+        type: Boolean,
+        onChange: () => {
+            location.reload();
+        }
+    });
+
+    game.settings.register("always-centred", "Button-OnOff", {
+        name: "Button - OnOff",
+        hint: "Toggles button visiblity",
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean,
+        onChange: () => {
+            location.reload();
+        }
+    });
+
+
+
+    game.settings.register("always-centred", "Button-PartyView", {
+        name: "Button - PartyView",
+        hint: "Toggles button visiblity",
+        scope: "client",
+        config: true,
+        default: false,
+        type: Boolean,
+        onChange: () => {
+            location.reload();
+        }
+    });
+
+    game.settings.register("always-centred", "Button-SelectedToken", {
+        name: "Button - SelectedToken",
+        hint: "Toggles button visiblity",
+        scope: "client",
+        config: true,
+        default: false,
+        type: Boolean,
+        onChange: () => {
+            location.reload();
+        }
+    });
+
+    game.settings.register("always-centred", "Debug", {
+        name: "Debug",
+        hint:"",
+        scope: "client",
+        config: true,
+        default: true,
+        type: Boolean
+    });
+};
